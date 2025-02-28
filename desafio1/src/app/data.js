79b973da8fba526ec684f25b4be54a3017fe3bc9
DanielSotoo <ds4792405@gmail.com>
2025-02-28 13:35:02 -0600

    Productos
    En este apartado, cree los productos de Tecnologia que utilizare y mostrare en la tienda

---
+++ desafio1/src/app/data.js
@@ -0,0 +1,159 @@
+export const data=[
+    {
+        id:1,
+        title:'Laptop asus vivobook',
+        price: 1999,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada67bdfb18d80e8.jpg',
+        quantity:1
+    },
+    
+    {
+        id:2,
+        title:'Laptop acer nitro',
+        price:1599,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada679e4db3dc09a.jpg',
+        quantity:1
+    },
+    
+    {
+        id:3,
+        title:'Laptop hp',
+        price:599,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada67926824eff7c.jpg',
+        quantity:1
+    },
+    
+    
+    {
+        id:4,
+        title:'Laptop asus',
+        price:449.95,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada663bcf396fefe.jpg',
+        quantity:1
+    }, 
+    
+    {
+        id:5,
+        title:'Laptop asus zenbook', 
+        price:849, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada66d88c3b96846.jpg',
+        quantity:1
+    },
+    {
+        id:6,
+        title:'Laptop hp victus 15', 
+        price:999, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada6684368998204.jpg',
+        quantity:1
+    },
+
+    {
+        id:7,
+        title:'Laptop hp 14', 
+        price:499, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada667c3619e5ad9.jpg',
+        quantity:1
+    }, 
+    {
+        id:8,
+        title:'Procesador intel core i9',
+        price: 749,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada652dd5cf7aaa7.jpg',
+        quantity:1
+    },
+    
+    {
+        id:9,
+        title:'Procesador amd ryzen 7',
+        price:499,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada66fc17080aae0.jpg',
+        quantity:1
+    },
+    
+    {
+        id:10,
+        title:'Disipador de aire para procesador intel y amd noctua',
+        price:119,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada64752017a2e15.jpg',
+        quantity:1
+    },
+    
+    
+    {
+        id:11,
+        title:'Sistema de enfriamiento liquido lian li ga ii trinity',
+        price:159,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada6761cc53cefd1.jpg',
+        quantity:1
+    }, 
+    
+    {
+        id:12,
+        title:'SSistema de enfriamiento liquido msi mag coreliquid a13', 
+        price:99.95, 
+        urlImage:'https://apizd.zonadigitalsv.com/storage/productos/71WjORLxE6D3qwHkLrDZUT8VezlBVXMMZzqRF5Wi.jpg',
+        quantity:1
+    },
+    {
+        id:13,
+        title:'Memoria ram ddr4 kingston fury beast 32gb', 
+        price:119, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada64752ef398686.jpg',
+        quantity:1
+    },
+
+    {
+        id:14,
+        title:'Memoria ram adata ddr5 32gb', 
+        price:129.95, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada662c1745a673f.jpg',
+        quantity:1
+    }, 
+    {
+        id:15,
+        title:'Teclado inalambrico bluetooth mecanico logitech g915',
+        price: 229.95,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada6760526a980bc.jpg',
+        quantity:1
+    },
+    
+    {
+        id:16,
+        title:'Teclado mecanico inalambrico recargable logitech g515',
+        price:159.95,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada66f6e751d88ce.jpg',
+        quantity:1
+    },
+    
+    {
+        id:17,
+        title:'Mouse usb xpg slingshot 12kdpi botones programables mera edition',
+        price:45,
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada662038d45b70b.jpg',
+        quantity:1
+    },
+    
+    
+    {
+        id:18,
+        title:'Mouse inalambrico y bluetooth razer cobra pro rgb focus pro sensor 30k',
+        price:139.95,
+        urlImage:'https://apizd.zonadigitalsv.com/storage/productos/v6r7woWN7AN8AAFZswfK0UKWin6SKiRHyO7fhs8P.jpg',
+        quantity:1
+    }, 
+    
+    {
+        id:19,
+        title:'Headset inalambrico bluetooth astro a50 pc xbox ps4 ps5 ns base magnetica bat recargable', 
+        price:299.95, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada6776acb4c7df4.jpg',
+        quantity:1
+    },
+    {
+        id:20,
+        title:'Headset inalambricos hyperx cloud flight xbox series x/s', 
+        price:149.95, 
+        urlImage:'https://api.zonadigitalsv.com/storage/products/imagen_generada65a16e39df71a.jpg',
+        quantity:1
+    }
+];
